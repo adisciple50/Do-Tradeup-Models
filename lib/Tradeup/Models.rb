2880adbfaa1421b189b624923ad8ffcc740e31c0
Jason Crockett <JesusisLord33333@outlook.com>
2022-01-09 23:52:33 +0000

    Initial commit

---
+++ lib/Tradeup/Models.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "Models/version"
+
+module Tradeup
+  module Database
+    module Models
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ test/Do/Tradeup/Models_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Do::Tradeup::ModelsTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Do::Tradeup::Models::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end

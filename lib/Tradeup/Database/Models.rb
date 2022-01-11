a6f9b0809a3a9c70d05cb9e055bd5e2330216b66
Jason Crockett <JesusisLord33333@outlook.com>
2022-01-11 16:50:36 +0000

    pairs complete

--- lib/Tradeup/Database/Models.rb
+++ lib/Tradeup/Database/Models.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 
 require "Models/version"
+require 'mongoid'
 
 module Tradeup
   module Database
     module Models
       class Error < StandardError; end
-      # Your code goes here...
+
+      class Pair
+        include  Mongoid::Document
+
+        field :symbol_one, type: String
+        field :symbol_two, type: String
+        field :amount, type: Float, default: 0.0
+
+      end
+
     end
   end
 end

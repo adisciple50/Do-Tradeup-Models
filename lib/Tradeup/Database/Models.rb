e518c43bc0a342ee31b4a15490e812c953f109bf
Jason Crockett <JesusisLord33333@outlook.com>
2022-01-11 17:11:52 +0000

    models done

--- lib/Tradeup/Database/Models.rb
+++ lib/Tradeup/Database/Models.rb
@@ -9,10 +9,24 @@ module Tradeup
       class Error < StandardError; end
 
       class Pair
-        include  Mongoid::Document
+        include Mongoid::Document
 
         field :symbol_one, type: String
         field :symbol_two, type: String
+        field :amount, type: Float, default: 0.0
+
+      end
+
+      class Chain
+        include Mongoid::Document
+
+        field :symbol_one, type: String
+        field :swap_one, type: Float, default: 0.0
+        field :symbol_two, type: String
+        field :swap_two, type: Float, default: 0.0
+        field :symbol_three, type: String
+        field :swap_three, type: Float, default: 0.0
+        field :symbol_four, type: String
         field :amount, type: Float, default: 0.0
 
       end
